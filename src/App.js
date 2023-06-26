cf069fcb67e494b7fab7118c56cabdcb4b32b5ae
Priya Thakur <74084538+priyathakur1234@users.noreply.github.com>
2023-06-26 19:57:38 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <header>
+    <div id="header">
+    <h1>Articles</h1>
+    
+
+    </div>
+
+    <div id="all-articles">
+   <h2>Article #1</h2>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+
+   <h2>Article #2</h2>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+
+   <h2>Article #3</h2>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+   <p>blah blah blah</p>
+
+</div>
+
+
+
+   
+
+   
+
+   </header>
+  );
+}
+
+export default App;
